3083a5b1e2c80b5d1cf2fde340a4946600382de0
Anatoliy Vedyakin <modemfux@gmail.com>
2023-06-05 17:11:59 +0300

    Add file with constants, add normalizing functions for report generation

---
+++ net_operations/lib/constants.py
@@ -0,0 +1,47 @@
+NAT_CARDS_CX600 = {
+    'CR5DVSUF8010': {
+        'cpus': {
+            'cpu0': {
+                'cpu_id': '0',
+                'cpu_type': 'engine',
+                'nat_bw_lic': False,
+                'default_bw': 20
+                }
+            },
+        'pics': {
+            'pic0': {
+                'cpu_id': '0',
+                'cpu_type': 'card',
+                'default_bw': 40
+                }
+            }
+        },
+    'CR5DVSUFD010': {
+        'cpus': {
+            'cpu0': {
+                'cpu_id': '0',
+                'cpu_type': 'engine',
+                'nat_bw_lic': False,
+                'default_bw': 20
+                },
+            'cpu1': {
+                'cpu_id': '1',
+                'cpu_type': 'engine',
+                'nat_bw_lic': False,
+                'default_bw': 20
+                }
+            },
+        'pics': {
+            'pic0': {
+                'cpu_id': '0',
+                'cpu_type': 'card',
+                'default_bw': 40
+                },
+            'pic1': {
+                'cpu_id': '1',
+                'cpu_type': 'card',
+                'default_bw': 40
+                }
+            }
+        }
+    }
--- net_operations/lib/huawei_funcs.py
+++ net_operations/lib/huawei_funcs.py
@@ -1,3 +1,4 @@
+from net_operations.lib.constants import NAT_CARDS_CX600
 import copy
 import re
 
@@ -109,8 +110,8 @@ def get_huawei_nat_physical_loc(conn) -> dict:
         if phys:
             slot, type_, id_ = phys.groups()
             phys_nat_location[loc] = {'slot': slot,
-                                      'type': type_,
-                                      'id': id_,
+                                      'cpu_type': type_,
+                                      'cpu_id': id_,
                                       'sig': None}
 
     sig_output = conn.send_commands(['display service-instance-group'])
@@ -137,7 +138,7 @@ def get_huawei_nat_payload_stats(conn) -> list:
     '''
 
     # Defining regexps
-    main = r'slot: +(?P<slot_num>\d+) (?P<type>card|engine): +(?P<type_num>\d+)'
+    main = r'slot: +(?P<slot>\d+) (?P<cpu_type>card|engine): +(?P<cpu_id>\d+)'
     rx_pps = r'.*current receive packet speed\(pps\): +(?P<curr_rx_pps>\S+)'
     rx_bps = r'.*current receive packet bit speed\(bps\): +(?P<curr_rx_bps>\S+)'
     tx_pps = r'.*current transmit packet speed\(pps\): +(?P<curr_tx_pps>\S+)'
@@ -213,8 +214,8 @@ def get_huawei_nat_summary_statistic(conn, nat_phys_dict) -> dict:
     for serv_loc in nat_phys_dict.keys():
         res_dict[serv_loc] = {}
         slot = nat_phys_dict[serv_loc]['slot']
-        type_ = nat_phys_dict[serv_loc]['type'].lower()
-        cpu_id = nat_phys_dict[serv_loc]['id']
+        type_ = nat_phys_dict[serv_loc]['cpu_type'].lower()
+        cpu_id = nat_phys_dict[serv_loc]['cpu_id']
         command = f'display nat statistics table slot {slot} {type_} {cpu_id}'
         output = conn.send_commands(command)
         for reg in reg_list:
@@ -247,7 +248,7 @@ def get_huawei_nat_session_license(conn):
     res_dict = {'license': None,
                 'distribution': {'total': None, 'per_cpu': []}}
     r_usage = r'.*LCX6NATDS00 +(?P<used>\d+)/(?P<total>\d+)'
-    r_distr = (r' +(?P<slot>\d+) +(?P<cpu_id>\d)[(](?P<type>\S+)(?: +)?[)]'
+    r_distr = (r' +(?P<slot>\d+) +(?P<cpu_id>\d)[(](?P<cpu_type>\S+)(?: +)?[)]'
                r' +(?P<cur_sess_qty>\d+) M')
     r_total = (r' *Total[ ]*Size *:(?P<total>\d+).*\n'
                r' *Used[ ]*Size *:(?P<used>\d+).*\n'
@@ -279,17 +280,141 @@ def get_huawei_nat_cards(conn):
     lic_out = conn.send_commands('display current-conf conf license')
     for vsu in re.finditer(r_vsu, output):
         res_dict[vsu.group(1)] = {'board': None, 'pics': [],
-                                  'bw_license': None}
+                                  'bw_license': []}
     for slot in res_dict.keys():
-        r_lic = fr' *active nat bandwith-enhance slot {slot} engine (\d)'
+        r_lic = fr' *active nat bandwidth-enhance slot {slot} engine (\d)'
         output = conn.send_commands(f'display version slot {slot}')
         searched = re.search(r_vsuf, output)
-        lic_search = re.search(r_lic, lic_out)
-        if lic_search:
-            engine = lic_search.group(1)
-            res_dict[slot]['bw_license'] = f'engine {engine}'
+        lic_search = re.finditer(r_lic, lic_out)
+        for lic_inst in lic_search:
+            engine = lic_inst.group(1)
+            res_dict[slot]['bw_license'].append(f'engine {engine}')
         if searched:
             res_dict[slot]['board'] = searched.group(1)
         for pic in re.finditer(r_pic, output):
             res_dict[slot]['pics'].append(pic.groups())
     return res_dict
+
+
+def huawei_nat_serv_loc_to_sig(nat_phy_dict) -> dict:
+    sig_dict = {
+         data['sig']: {'serv_loc': serv_loc,
+                       'slot': data['slot'],
+                       'cpu_type': data['cpu_type'],
+                       'cpu_id': data['cpu_id']
+                       } for serv_loc, data in nat_phy_dict.items()}
+    return sig_dict
+
+
+def fullreport_normalizing_start(cards_dict):
+    bras_dict = {'slots': {}}
+    for card in cards_dict.keys():
+        board = cards_dict[card]['board']
+        pics = cards_dict[card]['pics']
+        license = cards_dict[card]['bw_license']
+        bras_dict['slots'][card] = {'board': board, 'bw_license': license}
+        cpus = copy.deepcopy(NAT_CARDS_CX600[board]['cpus'])
+        bras_dict['slots'][card].update(cpus)
+        for lic in license:
+            lic = lic.replace('engine ', 'cpu')
+            bras_dict['slots'][card][lic]['nat_bw_lic'] = True
+        if pics:
+            for pic in pics:
+                pic_num, pic_card = pic
+                pic_num = pic_num.lower()
+                bras_dict['slots'][card][pic_num] = {'card': pic_card}
+                cons_v = copy.deepcopy(NAT_CARDS_CX600[board]['pics'][pic_num])
+                bras_dict['slots'][card][pic_num].update(cons_v)
+        else:
+            for pic_num in NAT_CARDS_CX600[board]['pics'].keys():
+                bras_dict['slots'][card][pic_num] = None
+    return bras_dict
+
+
+def fullreport_normalizing_phys(bras_dict, phys_dict):
+    for serv_loc, value in phys_dict.items():
+        slot = value['slot']
+        if value['cpu_type'] == 'engine':
+            name = 'cpu' + value['cpu_id']
+        else:
+            name = 'pic' + value['cpu_id']
+        bras_dict['slots'][slot][name]['serv_loc'] = serv_loc
+        bras_dict['slots'][slot][name]['sig'] = value['sig']
+
+
+def fullreport_normalizing_payload(bras_dict, payload_list):
+    keys = ['curr_rx_mbps', 'curr_rx_pps',
+            'curr_tx_mbps', 'curr_tx_pps',
+            'hist_max_rx_mbps', 'hist_max_rx_bps_date',
+            'hist_max_tx_mbps', 'hist_max_tx_bps_date']
+    for stats in payload_list:
+        stats_dict = {key: stats[key] for key in keys}
+        slot = stats['slot']
+        if stats['cpu_type'] == 'engine':
+            name = 'cpu' + stats['cpu_id']
+        else:
+            name = 'pic' + stats['cpu_id']
+        bras_dict['slots'][slot][name]['payload'] = copy.deepcopy(stats_dict)
+
+
+def fullreport_normalizing_sessions(bras_dict, sess_dict):
+    bras_dict['nat_session_license'] = copy.deepcopy(sess_dict['license'])
+    total = copy.deepcopy(sess_dict['distribution']['total'])
+    bras_dict['nat_session_qty'] = total
+    for cpu in sess_dict['distribution']['per_cpu']:
+        slot = cpu['slot']
+        if cpu['cpu_type'] == 'engine':
+            name = 'cpu' + cpu['cpu_id']
+        else:
+            name = 'pic' + cpu['cpu_id']
+        bras_dict['slots'][slot][name]['cur_sess_qty'] = int(
+            cpu['cur_sess_qty'])
+
+
+def fullreport_normalizing_instances(bras_dict, conf_dict, phy_dict):
+    sigs_dict = huawei_nat_serv_loc_to_sig(phy_dict)
+    for ni, data in conf_dict.items():
+        sig = data['sig']
+        slot = sigs_dict[sig]['slot']
+        cpu_id = sigs_dict[sig]['cpu_id']
+        cpu_type = sigs_dict[sig]['cpu_type']
+        if cpu_type == 'engine':
+            name = 'cpu' + cpu_id
+        else:
+            name = 'pic' + cpu_id
+        ni_dict = {ni: copy.deepcopy(data)}
+        bras_dict['slots'][slot][name].setdefault('instances',
+                                                  []).append(ni_dict)
+
+
+def fullreport_normalizing_summary(bras_dict, summ_dict, phy_dict):
+    sigs_dict = huawei_nat_serv_loc_to_sig(phy_dict)
+    for serv_loc, data in summ_dict.items():
+        sig = phy_dict[serv_loc]['sig']
+        slot = sigs_dict[sig]['slot']
+        cpu_id = sigs_dict[sig]['cpu_id']
+        cpu_type = sigs_dict[sig]['cpu_type']
+        if cpu_type == 'engine':
+            name = 'cpu' + cpu_id
+        else:
+            name = 'pic' + cpu_id
+        new_summ = copy.deepcopy(data)
+        bras_dict['slots'][slot][name].update(new_summ)
+
+
+def get_report_dict(conn):
+    phys = get_huawei_nat_physical_loc(conn)
+    cards = get_huawei_nat_cards(conn)
+    conf = get_huawei_nat_configuration(conn)
+    payload = get_huawei_nat_payload_stats(conn)
+    sess = get_huawei_nat_session_license(conn)
+    summ = get_huawei_nat_summary_statistic(conn, phys)
+
+    report_dict = fullreport_normalizing_start(cards)
+    fullreport_normalizing_phys(report_dict, phys)
+    fullreport_normalizing_payload(report_dict, payload)
+    fullreport_normalizing_sessions(report_dict, sess)
+    fullreport_normalizing_instances(report_dict, conf, phys)
+    fullreport_normalizing_summary(report_dict, summ, phys)
+
+    return report_dict
