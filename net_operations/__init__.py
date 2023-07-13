80c468b48b124b3b0fae4cd33305a36f5c3a3600
modemfux <39566858+modemfux@users.noreply.github.com>
2023-07-13 16:00:07 +0300

    Added possibility to direct import of classes

---
+++ net_operations/__init__.py
@@ -0,0 +1,4 @@
+from net_operations.lib.classes.NetOperations import NetOperations
+from net_operations.lib.classes.NetFiles import NetFiles
+from net_operations.lib.classes.NetDevices import NetDevices
+from net_operations.lib.classes.NetUser import NetUser
